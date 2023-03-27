b6564e260a858efbabe307d76c6c8f5879604515
praizoh <oremei.akande@gmail.com>
2023-03-27 20:45:27 +0100

    updates

--- src/views/components/Header.vue
+++ src/views/components/Header.vue
@@ -37,7 +37,7 @@
                         <i class="px-3 sm:hidden font-light flex ri-service-line"></i>
                         <a href="">Services</a>
                     </li>
-                    <li class="flex hover:bg-white py-1 sm:hover:bg-transparent hover:text-[#6750A4] transition ease-in-out duration-300 delay-300  sm:hidden flex  sm:hover:scale-110">
+                    <li class="flex hover:bg-white py-1 sm:hover:bg-transparent hover:text-[#6750A4] transition ease-in-out duration-300 delay-300  sm:hidden sm:hover:scale-110">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 px-3 mt-1  h-4">
                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
